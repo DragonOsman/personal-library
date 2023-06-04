f8d278857737eb5a459d66f3261567d2e102e0fa
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-05 02:41:34 +0300

    updated server code

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 connectDB();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 const books = require("./routes/api/books");
 const users = require("./routes/api/users");
@@ -18,12 +23,10 @@ app.use(cors({
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/users/", users);
+app.use(cookieParser());
+
+app.use("/api/users/", users);
 
 app.use("/api/books", books);
 
 app.get("/", (req, res) => res.send({ success: true }));
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
