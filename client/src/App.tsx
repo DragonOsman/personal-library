7f9bb7c140b3f89d751f49047ed87c72b69e9dbf
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-06 03:22:12 +0300

    deleted Home.tsx and useAuth hook from client side stuff, and auth middleware and config dir's db.js from server side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
+import Home from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <App />
+    </Router>
   );
 }
 
