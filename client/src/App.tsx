ca5474ce36050ec138521420d9dade25b057d82e
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-08 06:17:42 +0300

    removed stuff from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <h1>Hello</h1>
+    <div className="App">
+
+    </div>
   );
 }
 
