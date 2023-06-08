fe6d93024ae0627838b6d4f1904717188ba1c406
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-09 01:09:39 +0300

    imported Route and Routes into App.tsx and used them for Login routing; also completed Login code and added css file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
+import Login from "./components/Login";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   );
 }
--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,56 @@
+import { Link } from "react-router-dom";
+import "./Login.css";
+
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <div className="login-form-container">
+      <div className="col-10 col-sm-8 col-md-5 mx-auto">
+        <h1>Login</h1>
+      </div>
+      <form>
+        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
+          <legend>Email</legend>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="form-control form-control-lg"
+          />
+        </fieldset>
+        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
+          <legend>Password</legend>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="passord"
+            id="password"
+            className="form-control form-control-lg"
+          />
+        </fieldset>
+
+        <div className="col-10 col-sm-8 col-md-5 mx-auto">
+          <input
+            type="submit"
+            value="Login"
+            className="btn btn-lg btn-primary btn-block"
+          />
+        </div>
+        <div className="col-10 col-sm-8 col-md-5 mx-auto mt-5">
+          <p>
+            <Link to="/account/forgot" className="password-forgot">
+              I forgot my password
+            </Link>
+          </p>
+          <p className="register-cta">
+            Don't have an account?{" "}
+            <Link className="register" to="/register">
+              Register
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
   );
 };
 
