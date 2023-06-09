d77ec7223944647eb42bc339e33e87e4a0821a9a
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-09 21:08:58 +0300

    changed login form to one powered by formik like registration form

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,29 +1,47 @@
 import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 import "./Login.css";
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: ""
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("Please enter your email address"),
+      password: Yup.string().required("Please enter your password")
+    }),
+    onSubmit: async values => {
+      console.log(values);
+    }
+  });
+
   return (
     <div className="login-form-container">
       <div className="col-10 col-sm-8 col-md-5 mx-auto">
         <h1>Login</h1>
       </div>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
           <legend>Email</legend>
           <input
             type="email"
-            name="email"
             id="email"
             required
+            {...formik.getFieldProps("email")}
           />
         </fieldset>
         <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
           <legend>Password</legend>
           <input
             type="password"
-            name="passord"
             id="password"
             required
+            {...formik.getFieldProps("password")}
           />
         </fieldset>
 
