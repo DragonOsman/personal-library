6643555b87044d4fac4bb26576a3679a154402cb
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-10 01:02:34 +0300

    installed http-proxy-middle and used createProxyMiddleware to proxy backend routes to frontend

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = app => {
+  app.use(
+    ["/api/users/login", "/api/users/register", "/api/books/add-book", "/api/books/list-books",
+    "/api/books/show-book/:id", "api/books/update-book/:id", "/api/books/delete-book/:id"],
+    createProxyMiddleware({
+      target: "http://localhost:5000/"
+    })
+  );
+};
