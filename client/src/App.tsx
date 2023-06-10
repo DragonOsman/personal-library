f03d31d468ecf5f7843d2dd813baeb2c12bcd74a
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-10 20:44:28 +0300

    changed '/' route to show Home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import UserInfo from "./components/UserInfo";
+import Home from "./components/Home";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
@@ -10,7 +10,7 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<UserInfo />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
