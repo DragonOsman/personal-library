8ab8096c7538bf872238660a3e7340026b9c4759
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-10 23:26:31 +0300

    updated login and register components

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,72 +1,67 @@
-import { Link } from "react-router-dom";
-import * as Yup from "yup";
-import { useFormik } from "formik";
-import "./Login.css";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState, FormEvent } from "react";
 
 const Login = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: ""
-    },
-    validationSchema: Yup.object({
-      email: Yup.string()
-        .email("Invalid email address")
-        .required("Please enter your email address"),
-      password: Yup.string().required("Please enter your password")
-    }),
-    onSubmit: async values => {
-      console.log(values);
-    }
-  });
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const user = {
+      email,
+      password
+    };
+
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+
+    const data = await response.json();
+    localStorage.setItem("token", data.token);
+  };
+
+  useEffect(() => {
+    const verifyAuth = async () => {
+      const response = await fetch(`/api/users/is-user-auth`, {
+        headers: {
+          "x-access-token": String(localStorage.getItem("token"))
+        }
+      });
+      const data = await response.json();
+      if (data.isLoggedIn) {
+        navigate("/dashboard");
+      }
+    };
+
+    verifyAuth();
+  }, [navigate]);
 
   return (
-    <div className="login-form-container">
-      <div className="col-10 col-sm-8 col-md-5 mx-auto">
-        <h1>Login</h1>
-      </div>
-      <form onSubmit={formik.handleSubmit}>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-          <legend>Email</legend>
-          <input
-            type="email"
-            id="email"
-            required
-            {...formik.getFieldProps("email")}
-          />
-        </fieldset>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-          <legend>Password</legend>
-          <input
-            type="password"
-            id="password"
-            required
-            {...formik.getFieldProps("password")}
-          />
-        </fieldset>
-
-        <div className="col-10 col-sm-8 col-md-5 mx-auto">
-          <input
-            type="submit"
-            value="Login"
-            className="btn btn-lg btn-primary btn-block"
-          />
-        </div>
-        <div className="col-10 col-sm-8 col-md-5 mx-auto mt-5">
-          <p>
-            <Link to="/account/forgot" className="password-forgot">
-              I forgot my password
-            </Link>
-          </p>
-          <p className="register-cta">
-            Don't have an account?{" "}
-            <Link className="register" to="/register">
-              Register
-            </Link>
-          </p>
-        </div>
-      </form>
-    </div>
+    <form onSubmit={event => handleLogin(event)}>
+      <input
+        required
+        type="email"
+        name="email"
+        className="email"
+        value={email}
+        onChange={event => setEmail(event.target.value)}
+      />
+      <input
+        required
+        type="password"
+        name="password"
+        className="password"
+        value={password}
+        onChange={event => setPassword(event.target.value)}
+      />
+      <input type="submit" value="Login" />
+    </form>
   );
 };
 
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,141 +1,94 @@
 import "./Register.css";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState, FormEvent } from "react";
 
 const Register = () => {
-  const formik = useFormik({
-    initialValues: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      confirmPassword: ""
-    },
-    onSubmit: async (values): Promise<JSON | undefined> => {
-      try {
-        const response = await fetch("/api/users/register", {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json"
-          },
-          body: JSON.stringify(values)
-        });
-        if (response.status >= 200 && response.status < 400) {
-          return JSON.parse("You are successfully registered!");
-        } else {
-          JSON.parse("Something went wrong!");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const user = {
+      firstName,
+      lastName,
+      email,
+      password
+    };
+
+    await fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    });
+  };
+
+  useEffect(() => {
+    const verifyAuth = async () => {
+      const response = await fetch("/api/users/is-user-auth", {
+        headers: {
+          "x-access-token": String(localStorage.getItem("token"))
         }
-      } catch (err) {
-        console.log(err);
+      });
+      const data = await response.json();
+      if (data.isLoggedIn) {
+        navigate("/dashboard");
       }
-    },
-    validationSchema: Yup.object({
-      firstName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Please enter your first name"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Please enter your last name"),
-      email: Yup.string()
-        .email("Invalid email address")
-        .required("Please enter your email address"),
-      password: Yup.string()
-        .min(6, "Must be at least 6 characters")
-        .required("Please enter a password"),
-      confirmPassword: Yup.string()
-        .test("passwords-match", "Passwords must match", function(value: string | undefined): boolean {
-          return this.parent.password === value;
-        })
-        .required("Please retype the password")
-    })
-  });
+    };
+
+    verifyAuth();
+  }, [navigate]);
 
   return (
-    <div className="register-form-container">
-      <div className="col-10 col-sm-5 col-md-8 mx-auto">
-        <h1>Register</h1>
-      </div>
-
-      <form onSubmit={formik.handleSubmit}>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-          <legend>First Name</legend>
-          <input
-            type="text"
-            id="firstName"
-            required
-            {...formik.getFieldProps("firstName")}
-          />
-          {formik.touched.firstName && formik.errors.firstName ? (
-            <small className="form-text text-danger">
-              {formik.errors.firstName}
-            </small>
-          ) : null}
-        </fieldset>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-        <legend>Last Name</legend>
-          <input
-            type="text"
-            id="lastName"
-            required
-            {...formik.getFieldProps("lastName")}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <small className="form-text text-danger">
-              {formik.errors.lastName}
-            </small>
-          ) : null}
-        </fieldset>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-        <legend>Email</legend>
-          <input
-            type="email"
-            id="email"
-            required
-            {...formik.getFieldProps("email")}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <small className="form-text text-danger">
-              {formik.errors.email}
-            </small>
-          ) : null}
-        </fieldset>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-        <legend>Password</legend>
-          <input
-            type="password"
-            id="password"
-            required
-            {...formik.getFieldProps("password")}
-          />
-          {formik.touched.password && formik.errors.password ? (
-            <small className="form-text text-danger">
-              {formik.errors.password}
-            </small>
-          ) : null}
-        </fieldset>
-        <fieldset className="mb-3 col-10 col-sm-8 col-md-5 mx-auto mt-5">
-        <legend>Confirm Password</legend>
-          <input
-            type="password"
-            id="confirm-password"
-            required
-            {...formik.getFieldProps("confirm-password")}
-          />
-          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-            <small className="form-text text-danger">
-              {formik.errors.confirmPassword}
-            </small>
-          ) : null}
-        </fieldset>
-        <div className="col-10 col-sm-8 col-md-5 mx-auto mt-5">
-          <input
-            type="submit"
-            value="Register"
-            className="btn btn-lg btn-primary btn-block register-button"
-          />
-        </div>
-      </form>
-    </div>
+    <form onSubmit={event => handleRegister(event)}>
+      <input
+        type="text"
+        name="firstname"
+        className="first-name"
+        value={firstName}
+        required
+        onChange={event => setFirstName(event.target.value)}
+      />
+      <input
+        type="text"
+        name="lastname"
+        className="last-name"
+        value={lastName}
+        required
+        onChange={event => setLastName(event.target.value)}
+      />
+      <input
+        type="email"
+        name="email"
+        className="email"
+        value={email}
+        required
+        onChange={event => setEmail(event.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        className="password"
+        value={password}
+        required
+        onChange={event => setPassword(event.target.value)}
+      />
+      <input
+        type="password"
+        name="confirm-password"
+        className="password"
+        value={confirmPassword}
+        required
+        onChange={event => setConfirmPassword(event.target.value)}
+      />
+      <input type="submit" value="Register" />
+    </form>
   );
 };
 
