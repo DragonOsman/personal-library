f844feeb40a016374b75864ec22217dbc744e524
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-17 17:18:14 +0300

    added refreshToken route to setupProxy.js

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = app => {
   app.use(
     ["/api/users/login", "/api/users/register", "/api/books/add-book", "/api/books/list-books",
     "/api/books/show-book/:id", "api/books/update-book/:id", "/api/books/delete-book/:id",
-    "/api/users/user-info"],
+    "/api/users/user-info", "/api/users/refreshToken"],
     createProxyMiddleware({
       target: "http://localhost:5000/"
     })
