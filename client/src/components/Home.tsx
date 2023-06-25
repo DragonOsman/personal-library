ef0897e88d4ddf5dcce052ea0aa6219255ea47b1
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-26 02:48:25 +0300

    added user details fetching code to Home component

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,14 +1,45 @@
-import { useContext } from "react";
+import { useContext, useEffect, useCallback } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
+import BookList from "./BookList";
 import Loader from "../components/Loader";
-import UserInfo from "../components/UserInfo";
 
-// TODO: Fetch user data from backend and
-// greet the user with their name, then
-// show a link to their profile page which
-// would have their info and list of titles of books
 const Home = () => {
   const { userContext, setUserContext } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const previousUserContext = userContext;
+
+  const fetchUserDetails = useCallback(async () => {
+    const response = await fetch(
+      "https://personal-library-rvi3.onrender.com/api/users/user-info", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `jwt ${userContext.token}`
+      }
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setUserContext({ ...previousUserContext, details: data });
+    } else {
+      if (response.status === 401) {
+        // Edge case: when the token has expired.
+        // This could happen if the refreshToken calls have failed due to network error or
+        // User has had the tab open from previous day and tries to fetch data
+        navigate("/");
+      }
+    }
+  }, [previousUserContext, setUserContext, userContext.token, navigate]);
+
+  useEffect(() => {
+    // fetch only when user details are not present
+    if (!userContext.details) {
+      fetchUserDetails();
+    }
+  }, [fetchUserDetails, userContext.details]);
 
   return (
     userContext.details === null ? (
@@ -16,7 +47,24 @@ const Home = () => {
     ) : !userContext.details ? (
       <Loader />
     ) : (
-      <UserInfo />
+      <div className="user-details">
+        <p>
+          Welcome&nbsp;
+          <strong>
+            {userContext.details.firstName}
+            {userContext.details.lastName &&
+              " " + userContext.details.lastName}
+          </strong>!
+        </p>
+        {userContext.details.books.length > 0 ? (
+          <>
+            <h3>Below you can see your list of books:</h3>
+            <BookList />
+          </>
+        ) : (
+          <h3>Please add some books first!</h3>
+        )}
+      </div>
     )
   );
 };
