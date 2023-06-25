5370a379d4d8a9d91f66d80175e9d4ae344f9d89
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-25 23:07:50 +0300

    modified comment above passport.serializeUser call

--- strategies/LocalStrategy.js
+++ strategies/LocalStrategy.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 // Called during login/sign up.
 passport.use(new LocalStrategy(User.authenticate()));
 
-// called while after logging in / signing up to set user details in req.user
+// called after logging in / signing up to set user details in req.user
 passport.serializeUser(User.serializeUser());
 
 passport.deserializeUser(User.deserializeUser());
