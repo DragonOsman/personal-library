c81b78aa0353406ac5cf4dfb656b88a622a73ce8
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-29 02:32:37 +0300

    added bootstrap.js and took out bootstrap js bundle in index.tsx and changed value of aria-expanded to string 'false'

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -45,7 +45,7 @@ const Header = () => {
             type="button"
             data-bs-toggler="#responsive-navbar"
             aria-controls="responsive-navbar"
-            aria-expanded={!isCollapsed ? true : false}
+            aria-expanded="false"
             onClick={handleToggle}
             aria-label="Toggle navigation"
           >
