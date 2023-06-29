7258fdc5377ceec742f72656f39e9ce84c7336c7
Osman Zakir <osmanzakir90@hotmail.com>
2023-06-29 17:32:42 +0300

    changing access-control-allow-origin header value

--- routes/api/users.js
+++ routes/api/users.js
@@ -137,7 +137,7 @@ userRouter.post("/refreshToken", async (req, res, next) => {
           try {
             await user.save();
             res.setHeader("Content-Type", "application/json");
-            res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.app)$/);
+            res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.vercel.app)$/);
             res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS);
             res.json({ success: true, token, user });
           } catch (err) {
@@ -165,7 +165,7 @@ userRouter.post("/refreshToken", async (req, res, next) => {
 // @desc Send user details
 // @access Public
 userRouter.get("/user-info", verifyUser, (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.app)$/);
+  res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.vercel.app)$/);
   res.json({ success: true, user: req.user });
 });
 
@@ -188,7 +188,7 @@ userRouter.get("/logout", verifyUser, async (req, res, next) => {
 
     try {
       await user.save();
-      res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.app)$/);
+      res.setHeader("Access-Control-Allow-Origin", /^(https:\/\/){1}[a-z1-9-]+(.vercel.app)$/);
       res.clearCookie("refreshToken", COOKIE_OPTIONS);
       res.json({ success: true });
     } catch (err) {
