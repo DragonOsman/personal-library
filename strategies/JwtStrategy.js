95cc27c6cb7c75aaa8daebc8211328e798e36323
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 02:44:40 +0500

    added file in again

---
+++ strategies/JwtStrategy.js
@@ -0,0 +1,22 @@
+const passport = require("passport");
+const JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+const User = require("../models/User");
+const { JWT_SECRET } = process.env;
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("Bearer");
+opts.secretOrKay = JWT_SECRET;
+
+passport.use(
+  new JwtStrategy(opts, async (jwt_payload, done) => {
+    try {
+      const user = await User.findOne({ _id: jwt_payload._id });
+      if (user) {
+        return done(null, user);
+      }
+    } catch (err) {
+      return done(null, false);
+    }
+  })
+);
