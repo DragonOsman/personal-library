0a419b806d09e8f8472bf33cb16955dc8f78cd19
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 03:43:48 +0500

    changed code back to sending access token in response in register route, without storing refresh token in cookie

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const validateRegisterInput = require("../user-validation/register");
 const validateLoginInput = require("../user-validation/login");
+const { getToken } = require("../authenticate");
 
 userRouter.post("/register", (req, res) => {
   try {
@@ -23,16 +24,37 @@ userRouter.post("/register", (req, res) => {
       } else if (errors.lastName) {
         res.json({ error: errors.lastName });
       }
+    } else {
+      User.register(new User({
+        username: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      }, req.body.password,
+      async (err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.json({ error: err });
+          return;
+        } else {
+          const token = getToken({ _id: user._id });
+          try {
+            await user.save();
+            res.json({ success: true, message: "Registered", token: `Bearer ${token}` });
+          } catch (err) {
+            res.statusCode = 500;
+            res.json({ error: err });
+          }
+        }
+      }));
     }
-    const user = User.register(new User({
-      username: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName
-    }, req.body.password));
-    res.json({ success: true, message: "Successful", user });
   } catch (err) {
     res.json({ err });
   }
+});
+
+userRouter.post("/login", passport.authenticate("local", { session: false }),
+    async (req, res, next) => {
+
 });
 
 module.exports = userRouter;
