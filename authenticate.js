fcfdf88830f639b449ec7a038015d6821d07b2b8
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 04:27:22 +0500

    added COOKIE_OPTIONS back in

--- authenticate.js
+++ authenticate.js
@@ -3,6 +3,14 @@ const jwt = require("jsonwebtoken");
 const math = require("mathjs");
 const { SESSION_EXPIRY, JWT_SECRET } = process.env;
 
+exports.COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: true,
+  signed: true,
+  maxAge: math.evaluate(SESSION_EXPIRY) * 1000,
+  sameSite: "strict"
+};
+
 exports.getToken = user => {
   return jwt.sign(user, JWT_SECRET, {
     expiresIn: math.evaluate(SESSION_EXPIRY)
