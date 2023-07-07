db6e6b2c32af0961f622df678073ce2d25111e29
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 22:21:08 +0500

    added specialized cookie options constants for access and refresh tokens, and added refresh token getter function back in

--- authenticate.js
+++ authenticate.js
@@ -1,20 +1,36 @@
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const math = require("mathjs");
-const { SESSION_EXPIRY, JWT_SECRET } = process.env;
+const { SESSION_EXPIRY, JWT_SECRET, REFRESH_TOKEN_EXPIRY, REFRESH_TOKEN_SECRET } = process.env;
 
 exports.COOKIE_OPTIONS = {
   httpOnly: true,
   secure: true,
   signed: true,
-  maxAge: math.evaluate(SESSION_EXPIRY) * 1000,
   sameSite: "strict"
+};
+
+exports.ACCESS_TOKEN_COOKIE_OPTIONS = {
+  ...this.COOKIE_OPTIONS,
+  maxAge: math.evaluate(SESSION_EXPIRY) * 1000
+};
+
+exports.REFRESH_TOKEN_COOKIE_OPTIONS = {
+  ...this.COOKIE_OPTIONS,
+  maxAge: math.evaluate(REFRESH_TOKEN_EXPIRY) * 1000
 };
 
 exports.getToken = user => {
   return jwt.sign(user, JWT_SECRET, {
     expiresIn: math.evaluate(SESSION_EXPIRY)
   });
+};
+
+exports.getRefreshToken = user => {
+  const refreshToken = jwt.sign(user, REFRESH_TOKEN_SECRET, {
+    expiresIn: math.evaluate(REFRESH_TOKEN_EXPIRY)
+  });
+  return refreshToken;
 };
 
 exports.verifyUser = passport.authenticate("jwt", { session: false });
