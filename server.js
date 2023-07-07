6374799e02f8e870aae35ba78dbe059ec6c2eace
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 19:19:46 +0500

    removed trailing slash from userRouter app.use path

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.use(csrfProtection);
 
 app.get("/csrf-token", (req, res) => res.json({ csrfToken: req.csrfToken() }));
 
-app.use("/users/", userRouter);
+app.use("/users", userRouter);
 app.use("/api/books", bookRouter);
 
 app.get("/", (req, res) => res.json({ status: "success" }));
