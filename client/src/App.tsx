5755c115007737e3cacc3ba022a6a8dabcf90646
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-08 02:12:22 +0500

    added function checkJwtToken in useEffect to get access token and check to see if user is logged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,38 @@ import Register from "./components/Register";
 import Home from "./components/Home";
 import Loader from "./components/Loader";
 import { Route, Routes } from "react-router-dom";
-import { UserContext } from "./context/UserContext";
-import { useContext } from "react";
+import { useEffect, useContext } from "react";
+import { TokenContext } from "./context/TokenContext";
 import "./App.css";
 
 function App() {
-  const { userContext, setUserContext } = useContext(UserContext);
+  const { tokenData, setTokenData } = useContext(TokenContext);
+
+  // To check if user is logged in
+  useEffect(() => {
+    const checkJwtToken = async () => {
+      const response:Response = await fetch("/users/accessToken", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      const data = await response.json();
+      if (data.success && data.accessToken) {
+        setTokenData(data.token);
+      }
+    };
+
+    checkJwtToken();
+  }, [setTokenData]);
 
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={userContext.token === null ? <Login /> : (
-          userContext.token ? <Home /> : <Loader />)} />
+        <Route path="/" element={!tokenData ? <Login /> : (
+          tokenData ? <Home /> : <Loader />)} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
