fcfabfead4705f499b912c3407a8650b71ffbf25
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-08 01:47:59 +0500

    added context to hold access token

---
+++ client/src/context/TokenContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, ReactNode } from "react";
+
+interface AccessToken {
+  tokenData: string
+};
+
+const accessToken:AccessToken = {
+  tokenData: ""
+};
+
+interface TokenProviderProps {
+  children: ReactNode
+};
+
+const TokenContext = createContext<AccessToken>(accessToken);
+
+export const TokenContextProvider = ({ children }: TokenProviderProps) => {
+  return (
+    <TokenContext.Provider value={ { tokenData: accessToken.tokenData } }>
+      {children}
+    </TokenContext.Provider>);
+};
