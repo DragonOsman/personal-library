33e20ffd4de20c9588f29fbea83de906aeb19a7d
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-07 17:00:46 +0500

    added user info route, and also added return statements in !isvalid cases

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const validateRegisterInput = require("../user-validation/register");
 const validateLoginInput = require("../user-validation/login");
-const { getToken, COOKIE_OPTIONS } = require("../authenticate");
+const { getToken, COOKIE_OPTIONS, verifyUser } = require("../authenticate");
 
 userRouter.post("/register", (req, res) => {
   try {
@@ -15,14 +15,19 @@ userRouter.post("/register", (req, res) => {
       res.statusCode = 400;
       if (errors.password) {
         res.json({ error: errors.password });
+        return;
       } else if (errors.confirmPassword) {
         res.json({ error: errors.confirmPassword });
+        return;
       } else if (errors.email) {
         res.json({ error: errors.email });
+        return;
       } else if (errors.firstName) {
         res.json({ error: errors.firstName });
+        return;
       } else if (errors.lastName) {
         res.json({ error: errors.lastName });
+        return;
       }
     } else {
       User.register(new User({
@@ -61,8 +66,10 @@ userRouter.post("/login", passport.authenticate("local", { session: false }),
       res.statusCode = 400;
       if (errors.email) {
         res.json({ error: errors.email });
+        return;
       } else if (errors.password) {
         res.json({ error: errors.password });
+        return;
       }
     }
     const user = await User.findById(req.user._id);
@@ -72,7 +79,10 @@ userRouter.post("/login", passport.authenticate("local", { session: false }),
   } catch (err) {
     res.statusCode = 500;
     res.json({ error: err });
+    return next(err);
   }
 });
+
+userRouter.get("/user-info", verifyUser, (req, res, next) => res.json({ user: req.user }));
 
 module.exports = userRouter;
