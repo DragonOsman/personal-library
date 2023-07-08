a1b22ce3c92f11114e1c23e7d5b31f80f92abbe4
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-08 21:21:52 +0500

    changed user.refreshToken to invidividual object instead of array, and updated login, logout and refreshToken routes to reflech change

--- routes/api/users.js
+++ routes/api/users.js
@@ -86,7 +86,7 @@ userRouter.post("/login", passport.authenticate("local", { session: false }),
     }
     const user = await User.findById(req.user._id);
     if (user) {
-      user.refreshTokens.push({ refreshToken });
+      user.refreshToken = { refreshToken };
       try {
         await user.save();
         res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
@@ -109,10 +109,14 @@ userRouter.post("/login", passport.authenticate("local", { session: false }),
 userRouter.get("/user-info", verifyUser, (req, res, next) => res.json({ user: req.user }));
 
 userRouter.get("/logout", verifyUser, async (req, res, next) => {
+  const refreshToken = req.signedCookies.refreshToken;
   try {
     const user = await User.findById(req.user._id);
     if (user) {
-      user.refreshTokens = [];
+      if (user.refreshToken === refreshToken) {
+        user.refreshToken = {};
+      }
+
       try {
         await user.save();
         res.clearCookie("refreshToken", COOKIE_OPTIONS);
@@ -135,18 +139,13 @@ userRouter.post("/refreshToken", async (req, res, next) => {
       const payload = jwt.verify(refreshToken, REFRESH_TOKEN_SECRET);
       const userId = payload._id;
       const user = await User.findOne({ _id: userId });
+      console.log(`refreshToken route, refreshToken looks like this: ${refreshToken}
+                   user.refreshToken looks like this: ${user.refreshToken}`);
       if (user) {
-        const tokenIndex = user.refreshTokens.findIndex(
-          item => item.refreshToken === refreshToken
-        );
-
-        if (tokenIndex === -1) {
-          res.statusCode = 401;
-          res.json({ message: "Unauthorized" });
-        } else {
+        if (refreshToken === user.refreshToken) {
           const token = getToken({ _id: userId });
           const newRefreshToken = getRefreshToken({ _id: userId });
-          user.refreshTokens[tokenIndex] = { refreshToken: newRefreshToken };
+          user.refreshToken = { refreshToken: newRefreshToken };
           try {
             await user.save();
             res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS);
