78c746b2ef9ca6a01820ab8572857a6c891d7923
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-08 05:09:52 +0500

    added http-proxy-middleware, another attempt

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const express = require("express");
+const app = express();
+
+app.use([
+  "/users/refreshToken", "/users/accessToken", "/users/user-info", "/users/csrf-token",
+  "/users/register", "/users/login", "/users/logout", "/api/books/*"
+], createProxyMiddleware({
+  target: "https://personal-library-server.onrender.com",
+  changeOrigin: true
+}));
