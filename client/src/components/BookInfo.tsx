002e2ce59310b45a9795c10e34da8e273779774c
Osman Zakir <osmanzakir90@hotmail.com>
2023-07-11 02:34:56 +0500

    added BookInfo.tsx along with its css stylesheet

---
+++ client/src/components/BookInfo.tsx
@@ -0,0 +1,21 @@
+import { IBook } from "../context/UserContext";
+import "./BookInfo.css";
+
+const BookInfo = (book: IBook) => {
+  return (
+    <div className="container container-fluid">
+      <img
+        src="https://images.unsplash.com/photo-1495446815901-a7297e633e8d"
+        alt="Books"
+        height={200}
+      />
+      <div className="desc">
+        <h2>{book.title}</h2>
+        <h3>{book.author}</h3>
+        <p>{book.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default BookInfo;
