8aae8ca263faa125a5cb982b42cd4a32eb2efdd8
Osman Zakir <osmanzakir90@hotmail.com>
2023-08-02 22:10:10 +0500

    defining verifyUser with useCallback outside useEffect callback

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,42 +7,42 @@ import AddBook from "./components/AddBook";
 import ShowBookDetails from "./components/ShowBookDetails";
 import UpdateBook from "./components/UpdateBook";
 import { Route, Routes } from "react-router-dom";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useCallback } from "react";
 import { UserContext } from "./context/UserContext";
 import "./App.css";
 
 function App() {
   const { userContext, setUserContext } = useContext(UserContext);
 
-  useEffect(() => {
-    const verifyUser = async () => {
-      const previousUserContext = userContext;
-      try {
-        const response = await fetch(
-        "https://personal-library-server.onrender.com/api/users/refreshToken", {
-          method: "POST",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-          }
-        });
-
-        if (response.ok) {
-          const data = await response.json();
-          setUserContext({ ...previousUserContext, token: data.token });
-        } else {
-          setUserContext({ ...previousUserContext, token: null });
+  const verifyUser = useCallback(async () => {
+    const previousUserContext = userContext;
+    try {
+      const response = await fetch(
+      "https://personal-library-server.onrender.com/api/users/refreshToken", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
         }
-      } catch (err) {
-        console.log(`Error while making request to refreshToken route: ${err}`);
-      }
-    };
+      });
 
-    verifyUser();
+      if (response.ok) {
+        const data = await response.json();
+        setUserContext({ ...previousUserContext, token: data.token });
+      } else {
+        setUserContext({ ...previousUserContext, token: null });
+      }
+    } catch (err) {
+      console.log(`Error while making request to refreshToken route: ${err}`);
+    }
 
     // call verifyUser every 5mins
     setTimeout(verifyUser, 5 * 60 * 1000);
-  }, [userContext, setUserContext]);
+  }, [setUserContext, userContext]);
+
+  useEffect(() => {
+    verifyUser();
+  }, [verifyUser]);
 
   return (
     <>
