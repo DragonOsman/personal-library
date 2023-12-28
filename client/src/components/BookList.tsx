0f24677695c0b4b4b839cb8d947bfcb729e16ae6
Osman Zakir <osmanzakir90@hotmail.com>
2023-12-29 02:05:05 +0500

    increased top margin on list-title

--- client/src/components/BookList.tsx
+++ client/src/components/BookList.tsx
@@ -56,9 +56,10 @@ const BookList = () => {
       d-flex justify-content-center align-items-center">
       <div className="container-fluid col-auto">
         <div className="row container-fluid">
-          <h2 className="display-4 text-center col-auto list-title">
+          {isListVisible ? <h2 className="display-4 text-center col-auto list-title">
             Book List
-          </h2>
+          </h2> : <h2 className="display-4 text-center col-auto">Book List</h2>}
+
         </div>
         <div className="row container-fluid">
           <div>
