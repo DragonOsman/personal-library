0f0bafdf203dc986da034e67164d97825905d5fc
Osman Zakir <osmanzakir90@hotmail.com>
2024-01-04 20:52:01 +0500

    removing .list top margin & decreasing fetch/show list button bottom margin more

--- client/src/components/BookList.tsx
+++ client/src/components/BookList.tsx
@@ -80,7 +80,7 @@ const BookList = () => {
               className="btn btn-primary show-book-list"
               onClick={toggleVisibility}
               style={{
-                marginBottom: isListFetched && isListVisible ? "-100px" : ""
+                marginBottom: isListFetched && isListVisible ? "-500px" : ""
               }}
             >
               {isListVisible ? "Hide " : "Show "} Book List
