67de64569a9521637e34c88e5d926e892cd571a6
Osman Zakir <osmanzakir90@hotmail.com>
2024-07-11 21:26:04 +0500

    removed app.listen call and exported serverless function for vercel

--- server.js
+++ server.js
@@ -3,10 +3,28 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
+const CLIENT_URL = "https://personal-library-ejl3.onrender.com";
+const whitelist = [CLIENT_URL];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "Connection"],
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+  maxAge: 86400
+};
+app.use(cors(corsOptions));
 
 connectDB();
 
@@ -30,6 +48,4 @@ app.use("/api/books", bookRouter);
 
 app.get("/", (req, res) => res.json({ status: "success" }));
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
+module.exports = app;
