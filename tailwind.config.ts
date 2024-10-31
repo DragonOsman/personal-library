b1607e185ce71d204b38480c5fdeb515de6c973d
Osman Zakir <osmanzakir90@hotmail.com>
2024-10-31 20:23:51 +0500

    added some more linter rules & screen size defs in tailwind config theme property

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,22 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
     },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
