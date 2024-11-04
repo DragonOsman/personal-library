a1ff4a4c4f6f4b61e346a34a9860831de5490db7
Osman Zakir <osmanzakir90@hotmail.com>
2024-11-04 19:25:47 +0500

    added h2 element to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ import { redirect } from "next/dist/server/api-utils";
 import { prisma } from "./lib/prisma";
 
 const Home = () => {
-
+  return (
+    <h2>Welcome, Guest!</h2>
+  );
 };
 
 export default Home;
