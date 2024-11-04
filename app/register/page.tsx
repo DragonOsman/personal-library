88c5245f045695aa8bb59fa0707ee7e411c62150
Osman Zakir <osmanzakir90@hotmail.com>
2024-11-05 02:31:46 +0500

    removed registration form and import line

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,10 +1,7 @@
-import { RegisterForm } from "../features/auth/components/register-form";
-
 const Register = () => {
   return (
     <>
       <h2>Register</h2>
-      <RegisterForm />
     </>
   );
 };
