368811ecd8af7077f5cfd91dd223a9c697e2272e
Osman Zakir <osmanzakir90@hotmail.com>
2024-11-05 01:55:02 +0500

    added user registration page with form

---
+++ app/register/page.tsx
@@ -0,0 +1,12 @@
+import { RegisterForm } from "../features/auth/components/register-form";
+
+const Register = () => {
+  return (
+    <>
+      <h2>Register</h2>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
