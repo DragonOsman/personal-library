cacb07c83431d942ab88ceb875122a4e1cc02275
Osman Zakir <osmanzakir90@hotmail.com>
2024-11-13 14:55:43 +0500

    added some config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: false
+  },
+  webpack: (config) => {
+    config.modules.rules.push({
+      test: /\.tsx?$/,
+      use: {
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true
+        }
+      }
+    });
+    return config;
+  }
 };
 
 export default nextConfig;
