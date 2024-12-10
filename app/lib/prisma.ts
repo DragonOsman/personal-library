d3006f7acc414b8560123a4007273521d58197ca
Osman Zakir <osmanzakir90@hotmail.com>
2024-12-11 02:56:22 +0500

    calling prismaClient on line 11

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -8,7 +8,7 @@ declare const globalThis: {
   prismaGlobal: ReturnType<typeof prismaClient>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClient;
+const prisma = globalThis.prismaGlobal ?? prismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prismaGlobal = prisma;
