4391c3f2c14d404c7aff333479d70c9e066a869d
Osman Zakir <osmanzakir90@hotmail.com>
2024-12-19 20:43:00 +0500

    added missing parentheses on PrismaClient constructor, renamed singleton function to have 'Singleton' at the end, and adding function call parentheses on call on line 11

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,17 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = () => {
-  return new PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClient>;
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
 } & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClient;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prismaGlobal = prisma;
 }
-
-export default prisma;
