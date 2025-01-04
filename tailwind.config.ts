9e9c207b96a08d835168edbf05547150df118bcd
Osman Zakir <osmanzakir90@hotmail.com>
2025-01-05 04:44:26 +0500

    removed trailing commas and updated background and foreground colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,15 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)"
-      },
+        background: "var(--color-primary)",
+        foreground: "var(--color-secondary)"
+      }
     },
     screens: {
       xs: "300px",
