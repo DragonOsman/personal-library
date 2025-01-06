dca990ba96be3423a761278a8e2fa6b1b8a911a2
Osman Zakir <osmanzakir90@hotmail.com>
2025-01-06 14:20:42 +0500

    changing screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,11 +13,12 @@ const config: Config = {
       }
     },
     screens: {
-      xs: "300px",
-      sm: "480px",
+      xs: "500px",
+      sm: "640px",
       md: "768px",
-      lg: "976px",
-      xl: "1440px"
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px"
     }
   }
 };
