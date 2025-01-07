05570f3c72210efdc81002461c3fb4cce0aadf64
Osman Zakir <osmanzakir90@hotmail.com>
2025-01-07 20:25:45 +0500

    deleted prisma.ts added mysql db script and config file

---
+++ library_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE library_db;
+USE library_db;
+
+CREATE TABLE IF NOT EXISTS books (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  author VARCHAR(255) NOT NULL,
+  synopsis VARCHAR(1000),
+  isbn VARCHAR(13) NOT NULL,
+  publication_date TIMESTAMP
+)
