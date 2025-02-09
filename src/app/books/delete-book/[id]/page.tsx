68128a274d17a683f062ae55e1ba7dd824838e85
Osman Zakir <osmanzakir90@hotmail.com>
2025-02-10 03:11:23 +0500

    fixed dynamic routing for book deletion

---
+++ src/app/books/delete-book/[id]/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useContext, useEffect, useState } from "react";
+import { BookContext } from "@/src/app/context/BookContext";
+
+const DeleteBookPage = ({ params }: { params: Promise<{ id: string }> }) => {
+  const { books, setBooks } = useContext(BookContext);
+  const [id, setId] = useState("");
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  useEffect(() => {
+    const initId = async () => {
+      const newId = (await params).id;
+      setId(newId);
+    };
+
+    initId();
+  }, [params]);
+
+  const handleDelete = async () => {
+    const response = await fetch(`${baseUrl}/api/books/delete/:${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      credentials: "include"
+    });
+    if (response.ok) {
+      setBooks(books.filter((book) => book.id !== id));
+      console.log("Book deleted successfully");
+    }
+  };
+
+  return (
+    <div className="DeleteBook">
+      <h1>Delete A Book</h1>
+      <ul>
+        <>
+          {books.map((book) => (
+            <li key={book.id}>
+              {book.title}
+              <button
+                type="button"
+                onClick={() => handleDelete()}
+              >
+                Delete
+              </button>
+            </li>
+          ))}
+          {books.length === 0 && (
+            <li>
+              <p>No books to delete</p>
+            </li>
+          )}
+        </>
+      </ul>
+    </div>
+  );
+};
+
+export default DeleteBookPage;
