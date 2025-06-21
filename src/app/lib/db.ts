b26bd59e0ff4c4231c56418cc042fe6734c5af2e
Osman Zakir <osmanzakir90@hotmail.com>
2025-06-22 04:26:37 +0500

    installed vercel cli serverless neon driver + some other changes

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,6 +1,10 @@
 import { Pool } from "pg";
+import { neon } from "@neondatabase/serverless"
 
 const isProduction = process.env.NODE_ENV === "production";
+neon("postgres://neondb_owner:D8Amknrls7BR@ep-royal-firefly-a4ost6fl-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require", {
+  fullResults: true
+});
 
 const pool = new Pool({
   host: process.env.DATABASE_PGHOST,
