33dbc1f570a5ed06d1fa52989d726828248e4efc
Osman Zakir <osmanzakir90@hotmail.com>
2025-08-29 02:25:54 +0500

    added route file for next-auth config

---
+++ src/app/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+
