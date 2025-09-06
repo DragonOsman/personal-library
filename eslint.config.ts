7a92e9d02339245c6cfff3deb0c80ac0c3338c23
Osman Zakir <osmanzakir90@hotmail.com>
2025-09-06 22:42:13 +0500

    switched to eslint.config.ts

---
+++ eslint.config.ts
@@ -0,0 +1,59 @@
+// eslint.config.ts
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import react from "eslint-plugin-react";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  // Base JS recommended rules
+  js.configs.recommended,
+
+  // TypeScript recommended rules
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    plugins: {
+      react,
+      "jsx-a11y": jsxA11y,
+      "react-hooks": reactHooks,
+      "@next/next": nextPlugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+          arrowFunctions: true,
+          destructuring: true,
+          templateStrings: true,
+        },
+        projectService: true,
+      },
+    },
+    rules: {
+      yoda: "error",
+      "prefer-const": "error",
+      "space-before-function-paren": "off",
+      "@typescript-eslint/await-thenable": "warn",
+      "no-undef": "warn",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      quotes: ["error", "double", { allowTemplateLiterals: true }],
+      semi: "error",
+      "no-extra-semi": "error",
+      "comma-dangle": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
