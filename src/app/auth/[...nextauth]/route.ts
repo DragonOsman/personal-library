3b85498b3728051c357245452f1fdd7faae764bd
Osman Zakir <osmanzakir90@hotmail.com>
2025-09-07 21:14:50 +0500

    added missing semicolons

--- src/app/auth/[...nextauth]/route.ts
+++ src/app/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
-import { handlers } from "../../../auth"
-import prisma from "@prisma/client"
+import { handlers } from "../../../auth";
+import prisma from "@prisma/client";
 import { PrismaClient } from "../../../generated/prisma";
 import Nodemailer from "next-auth/providers/nodemailer";
 import bcrypt from "bcryptjs";
