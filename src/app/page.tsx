1821b5b9c48a95bfdb70399250c78da99441e6b4
Osman Zakir <osmanzakir90@hotmail.com>
2025-09-25 19:55:46 +0500

    removed clerk import and fixed import paths for own signup/signin using auth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { SignIn } from "@clerk/nextjs";
 import BookList from "@/src/app/books/list-books/page";
+import { auth } from "@/src/auth";
+import SignIn from "@/src/app/components/Signin";
 
 const Page = async () => {
-  const user = await currentUser();
+  const session = await auth();
+  if (!session) {
+    return <SignIn />;
+  }
+
+  const user = session.user;
   if (!user) {
     return <SignIn />;
   } else {
     return (
       <div className="Homepage">
-        <h1>Welcome, {user.fullName}</h1>
+        <h1>Welcome, {user.name}</h1>
         <BookList />
       </div>
     );
