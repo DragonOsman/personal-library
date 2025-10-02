0dadcaf6043490799a706e8771ddd999afddae25
Osman Zakir <osmanzakir90@hotmail.com>
2025-10-02 20:47:38 +0500

    leaving only handlers import, and exporting get and post handlers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,2 @@
-import NextAuth from "next-auth";
 import { handlers } from "../../../../auth";
-import prisma from "@prisma/client";
-import { PrismaClient } from "../../../../generated/prisma";
-import Nodemailer from "next-auth/providers/nodemailer";
-import bcrypt from "bcryptjs";
-import dotenv from "dotenv";
-dotenv.config();
-
+export const { GET, POST } = handlers;
