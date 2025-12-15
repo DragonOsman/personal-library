310b46bb6deafefe73a65796aa7fafd6d29a5dfa
Osman Zakir <osmanzakir90@hotmail.com>
2025-12-15 21:56:46 +0500

    replaced Head element with Metadata, and moved BookContextProvider to just above UserProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,50 @@
 import { ReactNode, FC } from "react";
 import Head from "next/head";
+import { Metadata } from "next";
 import Header from "./components/Header";
 import BookContextProvider from "./context/BookContext";
 import UserProvider from "./context/UserContext";
 import { SessionProvider } from "next-auth/react";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "DragonOsman Personal Library App",
+  description: "Next.js app implementing an app for users to keep a list of books they own or have read.",
+  keywords: [
+    "Next.js",
+    "TypeScript",
+    "Library",
+    "Personal Library",
+    "Authentication",
+    "User Auth",
+    "Auth",
+    "Auth.js",
+    "PostgreSQL",
+    "Books"
+  ],
+  icons: "/images/favicon.ico"
+};
+
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <BookContextProvider>
-      <html lang="en-us">
-        <Head>
-          <title>DragonOsman Personal Library App</title>
-          <meta name="description" content="Next.js app implementing an app for users to keep a list of books they own or have read." />
-          <meta charSet="utf-8" />
-          <meta name="keywords" content="Next.js, TypeScript, Library, Authentication, User Auth, Auth, MySQL" />
-          <link rel="icon" href="/public/images/favicon.ico" />
-        </Head>
-        <body
-          className="mt-20 text-[17px] flex flex-col min-h-screen text-center items-center justify-center text-[color:var(--color-text)] font-sans"
-        >
-          <SessionProvider>
+    <html lang="en-us">
+      <body
+        className="mt-20 text-[17px] min-h-screen text-center text-[color:var(--color-text)] font-sans"
+      >
+        <SessionProvider>
+          <BookContextProvider>
             <UserProvider>
               <Header />
-              <main className="flex flex-1 w-full items-center justify-center p-4">
-                {children}
+              <main className="flex flex-1 items-center justify-center p-4">
+                <div className="w-full max-w-3xl">
+                  {children}
+                </div>
               </main>
             </UserProvider>
-          </SessionProvider>
-        </body>
-      </html>
-    </BookContextProvider>
+          </BookContextProvider>
+        </SessionProvider>
+      </body>
+    </html>
   );
 };
 
