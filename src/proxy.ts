0b4dee3f400a57dae85bffca56d79618e6a5f340
Osman Zakir <osmanzakir90@hotmail.com>
2025-12-24 05:02:52 +0500

    trying to fix proxy function export

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,8 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
-import { GetServerSidePropsContext } from "next";
 
 const { auth } = NextAuth(authConfig);
 
-export function proxy(req: GetServerSidePropsContext) {
-  return auth(req);
-}
+const proxy = auth;
+
+export default proxy;
