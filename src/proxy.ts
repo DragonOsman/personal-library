f9696c3f20e4ee6b9047d95135308117223c20b8
Osman Zakir <osmanzakir90@hotmail.com>
2025-12-25 01:58:39 +0500

    fixed proxy function export

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,4 @@
-import NextAuth from "next-auth";
-import authConfig from "./auth.config";
-
-const { auth } = NextAuth(authConfig);
-
-export const proxy = auth;
+export { auth as proxy } from "@/src/auth";
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
