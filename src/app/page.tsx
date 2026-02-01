e53f1e2c6005beb836a0506af55d19c646f55eae
Osman Zakir <osmanzakir90@hotmail.com>
2026-02-02 04:53:34 +0500

    removed session check and user's name from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,12 @@
 import BookList from "@/src/app/books/list-books/page";
-import { auth } from "@/src/auth";
-import Signin from "@/src/app/components/Signin";
-import { NextRequest } from "next/server";
 
-const Page = async (req: NextRequest) => {
-  const session = await auth.api.getSession({
-    headers: req.headers
-  });
-  if (!session) {
-    return <Signin />;
-  }
-
-  const user = session.user;
-  if (!user) {
-    return <Signin />;
-  } else {
-    return (
-      <div className="Homepage">
-        <h1>Welcome, {user.name}</h1>
-        <BookList />
-      </div>
-    );
-  }
+const Page = async () => {
+  return (
+    <div className="Homepage">
+      <h1>Welcome</h1>
+      <BookList />
+    </div>
+  );
 };
 
 export default Page;
