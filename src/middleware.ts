2a52b70f2eccf1a8ef259ef5491b656b094735aa
Osman Zakir <osmanzakir90@hotmail.com>
2026-02-02 04:20:59 +0500

    deleted middleware and tried to fix proxy

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-export { proxy as middleware } from "@/src/proxy";
-
-export const config = {
-  runtime: "nodejs",
-  matcher: ["/", "/users/profile", "/books/:path*"]
-};
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,6 @@
 import { auth } from "./auth";
-import { headers } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
+export { auth as proxy };
 
-export const proxy = async (request: NextRequest) => {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  });
-
-  if (!session) {
-    return NextResponse.redirect(new URL("/auth/signin", request.url));
-  }
-  return NextResponse.next();
+export const config = {
+  matcher: ["/", "/users/profile", "/books/:path*"]
 };
