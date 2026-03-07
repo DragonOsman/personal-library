0bb2f9215bd7895ee230efa7c06139b5449ccbad
Osman Zakir <osmanzakir90@hotmail.com>
2026-03-07 05:45:11 +0500

    updated image import setting in next config & removed prisma migrate command from build step

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,14 @@ const nextConfig: NextConfig = {
     ]
   },
   images: {
-    domains: ["ui-avatars.com", "lh3.googleusercontent.com"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+        port: "",
+        pathname: "/api/*"
+      }
+    ]
   }
 };
 
