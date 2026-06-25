f3f773dd42672097f1b12c26e79252dc06ab5005
Osman Zakir <osmanzakir90@hotmail.com>
2026-06-25 22:07:55 +0500

    renamed SettingsPanel to SettingsClient

--- src/app/users/settings/page.tsx
+++ src/app/users/settings/page.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2026 Your Name
 // Licensed under the GPL v3
 
-import SettingsClient from "./SettingsPanel";
+import SettingsClient from "./SettingsClient";
 import { Metadata } from "next";
 import { getRouteTitle, getRouteKeywords } from "@/lib/routeTitles";
 
