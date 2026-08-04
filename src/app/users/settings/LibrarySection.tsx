dcbd0809773634b0eb3c85ed13ac4eb8b8d9a9d0
Osman Zakir <osmanzakir90@hotmail.com>
2026-08-04 21:29:26 +0500

    adding library section component page

---
+++ src/app/users/settings/LibrarySection.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function LibrarySection() {
+  return (
+    <div className="space-y-4">
+      <h3>Library Settings</h3>
+
+      <p>
+        Configure how your personal library is displayed and how new books are
+        added.
+      </p>
+    </div>
+  );
+}
