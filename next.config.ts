924af3662414946d739eea0b648042e6346c9632
Osman Zakir <osmanzakir90@hotmail.com>
2026-08-23 01:40:37 +0500

    added remotePatterns for more urls and added profile imagein user card

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,31 @@ const nextConfig: NextConfig = {
         hostname: "ui-avatars.com",
         port: "",
         pathname: "/api/**"
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/avatars/**"
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.discordapp.com",
+        pathname: "/embed/avatars/**"
+      },
+      {
+        protocol: "https",
+        hostname: "books.google.com",
+        pathname: "/books/content/**"
+      },
+      {
+        protocol: "https",
+        hostname: "books.googleusercontent.com",
+        pathname: "/**"
       }
     ]
   }
--- src/app/components/UserCard.tsx
+++ src/app/components/UserCard.tsx
@@ -18,20 +18,38 @@ interface UserCardProps {
 }
 
 export default function UserCard({ user, twoFactorEnabled }: UserCardProps) {
+
+  const userName = user.name?.trim() || "User";
+
+  const imageUrl =
+    user.image?.trim() ||
+    `https://ui-avatars.com/api/?name=${encodeURIComponent(
+      userName
+    )}&rounded=true&size=128`
+  ;
+
   return (
     <Card>
       <div className="space-y-4">
         <h2 className="text-xl font-semibold">Profile</h2>
 
+        <div className="flex justify-center">
+          <div className="avatar">
+            <div className="w-32 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
+              <Image
+                src={imageUrl}
+                alt="Profile Image"
+                width={128}
+                height={128}
+                className="h-8 w-8 rounded-full object-cover"
+              />
+            </div>
+          </div>
+        </div>
+
         <div>
           <p className="text-sm text-gray-500">Name</p>
           <p>{user.name}</p>
-        </div>
-        <div>
-          <Image
-            src={user.image!}
-            alt="Profile Image"
-          />
         </div>
         <div>
           <p className="text-sm text-gray-500">Email</p>
